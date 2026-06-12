48e68abb17a5289389b6c0715ad5bc8ba6c60454
Aditya Elayavalli <adityaelayavalli@Adityas-MacBook-Pro-2.local>
2026-06-12 16:21:03 -0500

    style check update

--- src/main/java/student/Greeting.java
+++ src/main/java/student/Greeting.java
@@ -1,10 +1,10 @@
 package student;
 
 /**
- * This class is a place holder which you will fully implement based on the javadoc
- * 
+ * This class is a place holder which you will fully implement based on the javadoc.
+ *
  * https://cs5004-khoury-lionelle.github.io/hello_world/student/package-summary.html
- * 
+ *
  */
 public class Greeting {
 
